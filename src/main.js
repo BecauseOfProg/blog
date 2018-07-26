562f82466bcca3f5ebf0fd7890cc8b930edc1239
Exybore <exybore@gmail.com>
2018-07-26 18:32:45 +0200

    🔥 style(*): remove useless comments
    Removing useless comments from documentations and project template.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import Resource from 'vue-resource'
@@ -19,10 +17,10 @@ Vue.component('b-intro-div', BIntroDiv)
 Vue.component('b-article-card', BArticleCard)
 
 Vue.use(Meta, {
-  keyName: 'metaInfo', // the component option name that vue-meta looks for meta info on.
-  attribute: 'data-vue-meta', // the attribute name vue-meta adds to the tags it observes
-  ssrAttribute: 'data-vue-meta-server-rendered', // the attribute name that lets vue-meta know that meta info has already been server-rendered
-  tagIDKeyName: 'vmid' // the property name that vue-meta uses to determine whether to overwrite or append a tag
+  keyName: 'metaInfo',
+  attribute: 'data-vue-meta',
+  ssrAttribute: 'data-vue-meta-server-rendered',
+  tagIDKeyName: 'vmid'
 })
 
 Vue.use(Resource)
