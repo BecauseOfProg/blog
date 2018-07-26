03e40c34807fc9f8f42250b7d7b9b7b5a5507857
Exybore <exybore@gmail.com>
2018-07-26 14:39:43 +0200

    💄 ui(*): new footer
    New footer with a little sitemap and copyright.

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,44 @@
   <v-app>
     <b-header></b-header>
     <router-view></router-view>
-    <v-footer app></v-footer>
+    <v-footer height="auto" color="primary">
+      <v-layout justify-center row wrap>
+        <v-btn v-for="link in links" :key="link.name" :to="link.link" color="white" flat round>
+          {{ link.name }}
+        </v-btn>
+        <v-flex primary darken-1 py-3 text-xs-center white--text xs12>
+          &copy;2018 — <strong>BecauseOfProg</strong>
+        </v-flex>
+      </v-layout>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data() {
+    return {
+      links: [
+        {
+          name: 'Accueil',
+          link: '/'
+        },
+        {
+          name: 'Articles',
+          link: '/articles'
+        },
+        {
+          name: 'A propos',
+          link: '/about'
+        },
+        {
+          name: 'Application',
+          link: '/about/app'
+        }
+      ]
+    }
+  }
 }
 </script>
 
