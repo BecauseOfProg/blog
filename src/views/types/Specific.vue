ff146b7a8c43d14dcbfb42fab059271c6f8a71f2
Exybore <exybore@gmail.com>
2018-07-27 19:13:44 +0200

    ✨ feat(types): specific types page
    Adding specific types page, with intro-div and layout.

---
+++ src/views/types/Specific.vue
@@ -0,0 +1,44 @@
+<template>
+  <main v-if="loaded">
+    <b-intro-div :title="type.name" :banner="type.banner" iconType="icon" :icon="type.icon"></b-intro-div>
+
+    <v-container fluid grid-list-md>
+      <v-layout row>
+        <v-container fluid>
+          <v-layout row wrap>
+            <v-flex xs12>
+              <v-layout row wrap>
+                
+              </v-layout>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-layout>
+    </v-container>
+  </main>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import vuex from 'vuex'
+
+export default {
+  data () {
+    return {
+      type: [],
+      loaded: false
+    }
+  },
+  computed: {
+    ...mapGetters(['getOneType'])
+  },
+  mounted () {
+    this.type = this.getOneType(this.$route.params.type)[0]
+    this.loaded = true
+  }
+}
+</script>
+
+<style>
+
+</style>
