f4cf08e4a140906899e45cb1c3297904219cc62b
Théo Vidal <exybore@becauseofprog.fr>
2020-06-10 22:34:10 +0200

    feat: add SocialIcons component for a quick integration in pages

--- src/utils/data/socials.js
+++ src/utils/data/socials.js
@@ -1,9 +1,9 @@
 const socials = [
-  { name: 'Twitter', icon: 'mdi-twitter', link: 'https://twitter.com/BecauseOfProg'},
-  { name: 'Discord', icon: 'mdi-discord', link: 'https://discord.becauseofprog.fr'},
-  { name: 'Mastodon', icon: 'mdi-mastodon', link: 'https://mstdn.io/@bop'},
-  { name: 'GitHub', icon: 'mdi-github-circle', link: 'https://github.com/BecauseOfProg'},
-  { name: 'Instagram', icon: 'mdi-instagram', link: 'https://instagram.com/BecauseOfProg'}
+  { name: 'Twitter', icon: 'mdi-twitter', color: 'light-blue', link: 'https://twitter.com/BecauseOfProg'},
+  { name: 'Discord', icon: 'mdi-discord', color: 'teal', link: 'https://discord.becauseofprog.fr'},
+  { name: 'Mastodon', icon: 'mdi-mastodon', color: 'blue', link: 'https://mstdn.io/@bop'},
+  { name: 'GitHub', icon: 'mdi-github', color: 'darker', link: 'https://github.com/BecauseOfProg'},
+  { name: 'Instagram', icon: 'mdi-instagram', color: 'pink', link: 'https://instagram.com/BecauseOfProg'}
 ]
 
 export { socials }
---
+++ src/views/parts/SocialIcons.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="social-icons social-icons--big">
+    <v-row class="mx-0">
+      <a
+        v-for="social in socials"
+        :key="social.link"
+        :href="social.link">
+        <v-icon
+          :size="big ? 96 : undefined"
+          :color="big ? social.color : 'darker'">
+          {{ social.icon }}
+        </v-icon>
+      </a>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import { socials } from '@/utils/data'
+
+export default {
+  name: 'SocialIcons',
+  props: {
+    big: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      socials
+    }
+  }
+}
+</script>
