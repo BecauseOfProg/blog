0f84edbcceccec4f47ffe30a158df56f4dbbfd99
Théo Vidal <exybore@becauseofprog.fr>
2020-06-10 22:36:08 +0200

    feat: add articles list view
    Added the articles list, that can adapt to categories and types and has an integrated pagination.

--- src/views/blog/List.vue
+++ src/views/blog/List.vue
@@ -1,11 +1,181 @@
 <template>
-  <b-top-banner
-    title="Articles"
-    icon="mdi-file-document-box-multiple-outline"/>
+  <main>
+    <b-top-banner
+      :title="head.title"
+      :icon="head.icon"
+      :src="head.image"
+      tall/>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          lg="7"
+          offset-lg="1">
+          <template v-if="loaded">
+            <v-pagination
+              v-model="page"
+              :length="Math.ceil(articles.length / 10)"
+              color="darker"/>
+            <v-row>
+              <v-col
+                v-for="article in articles.slice((page - 1) * 10, (page - 1) * 10 + 10)"
+                :key="article.url"
+                cols="12">
+                <b-article-card :article="article"/>
+              </v-col>
+            </v-row>
+          </template>
+          <template v-else>
+            <v-row>
+              <v-spacer/>
+              <v-progress-circular
+                indeterminate
+                color="darker"/>
+              <v-spacer/>
+            </v-row>
+          </template>
+        </v-col>
+        <v-col
+          cols="12"
+          lg="3">
+          <b-card
+            fluid
+            class="bordered">
+            <v-list
+              class="round"
+              subheader
+              dense
+              tile>
+              <v-list-item
+                :to="{ name: 'all-articles' }"
+                color="darker">
+                <v-list-item-icon>
+                  <v-icon>mdi-text-box-multiple-outline</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>Tous les articles</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-divider/>
+              <v-subheader>CATÉGORIES</v-subheader>
+              <v-list-item
+                v-for="category in categories"
+                :key="category.id"
+                :to="{ name: 'category', params: { category: category.id }}"
+                color="darker">
+                <v-list-item-icon>
+                  <v-icon>{{ category.icon }}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>{{ category.name }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-divider/>
+              <v-subheader>TYPES</v-subheader>
+              <v-list-item
+                v-for="type in types"
+                :key="type.id"
+                :to="{ name: 'type', params: { type: type.id }}"
+                color="darker">
+                <v-list-item-icon>
+                  <v-icon>{{ type.icon }}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>{{ type.name }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+            <v-divider/>
+            <div class="pa-3">
+              <span class="headline darker--text">Nous suivre</span>
+              <social-icons big/>
+            </div>
+            <v-divider/>
+            <div class="pa-3">
+              <span class="headline darker--text">À propos</span>
+              <p>BecauseOfProg, c'est des développeurs rassemblés autour de projets communs tels que le blog.</p>
+              <v-btn
+                text
+                color="darker"
+                :to="{ name: 'about' }">
+                <v-icon left>mdi-information-outline</v-icon>
+                En savoir plus
+              </v-btn>
+            </div>
+          </b-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </main>
 </template>
 
 <script>
+import { blogPosts } from '@/utils/api'
+import { categories, types, getCategory, getType } from '@/utils/data'
+import SocialIcons from '@/views/parts/SocialIcons'
+
 export default {
-  name: 'ArticleList'
+  name: 'ArticleList',
+  components: { SocialIcons },
+  data() {
+    return {
+      head: {
+        title: '',
+        icon: '',
+        image: undefined
+      },
+      articles: [],
+      loaded: false,
+      page: 1,
+
+      categories,
+      types
+    }
+  },
+  mounted() {
+    let page
+    if (this.$route.params.category) {
+      let category = getCategory(this.$route.params.category)
+      this.head.title = `Catégorie — ${category.name}`
+      this.head.icon = category.icon
+      this.head.image = `/img/category/${category.id}.png`
+      page = {
+        type: 'category',
+        data: category.id
+      }
+    } else if (this.$route.params.type) {
+      let type = getType(this.$route.params.type)
+      this.head.title = `Type — ${type.name}`
+      this.head.icon = type.icon
+      this.head.image = `/img/type/${type.id}.png`
+      page = {
+        type: 'type',
+        data: type.id
+      }
+    } else {
+      this.head.title = 'Tous les articles'
+      this.head.icon = 'mdi-text-box-multiple-outline'
+      page = {
+        type: 'all'
+      }
+    }
+
+    let articles
+    blogPosts.get().then(response => {
+      articles = response.body.data
+      switch (page.type) {
+      case 'category' :
+        this.articles = articles.filter(article => article.category === page.data)
+        break
+      case 'type' :
+        this.articles = articles.filter(article => article.type === page.data)
+        break
+      default :
+        this.articles = articles
+        break
+      }
+      this.loaded = true
+    })
+  }
 }
 </script>
