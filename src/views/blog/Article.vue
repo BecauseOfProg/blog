209f105da8e83798a30c059dc2991b88249562c2
Théo Vidal <exybore@becauseofprog.fr>
2020-06-11 21:47:23 +0200

    style: change article view to medium-like

--- src/views/blog/Article.vue
+++ src/views/blog/Article.vue
@@ -12,8 +12,7 @@
           <v-col
             class="text-center"
             cols="12">
-            <span class="display-3 white--text">{{ article.title }}</span><br>
-            <span class="headline grey--text">Publié par <strong>{{ article.author.displayname }}</strong> le {{ dateToText(article.timestamp) }}</span>
+            <span class="display-3 white--text">{{ article.title }}</span>
           </v-col>
         </v-row>
       </v-parallax>
@@ -21,14 +20,41 @@
         <v-row>
           <v-col
             cols="12"
-            lg="7"
-            offset-lg="1"
+            lg="8"
+            offset-lg="2"
             class="upper-content">
+            <b-card class="bordered">
+              <span style="font-size: 22px">« {{ article.description }} »</span>
+              <template #actions>
+                <v-tooltip
+                  v-for="share in shares"
+                  :key="share.name + 'badge'"
+                  bottom>
+                  <template #activator="{ on }">
+                    <v-btn
+                      :color="share.color"
+                      :href="share.link"
+                      target="_blank"
+                      icon
+                      v-on="on">
+                      <v-icon>{{ share.icon }}</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>{{ share.name }}</span>
+                </v-tooltip>
+                <v-spacer/>
+                <v-icon left>mdi-clock-outline</v-icon>
+                <span>Publié par <strong>{{ article.author.displayname }}</strong> le {{ dateToText(article.timestamp) }}</span>
+              </template>
+            </b-card>
+          </v-col>
+          <v-col
+            cols="12"
+            lg="6"
+            offset-lg="3">
             <b-card
               id="article-content"
               class="bordered">
-              <span class="headline">{{ article.description }}</span>
-              <v-divider/>
               <vue-markdown class="markdown-body">{{ article.content }}</vue-markdown>
               <v-col class="text-center">
                 <span class="headline">Partager l'article</span><br>
@@ -37,6 +63,7 @@
                   :key="share.name"
                   :color="share.color"
                   :href="share.link"
+                  target="_blank"
                   class="ma-1"
                   outlined>
                   <v-icon left>{{ share.icon }}</v-icon>
@@ -59,84 +86,102 @@
               </v-col>
             </b-card>
           </v-col>
-          <v-col
-            cols="12"
-            lg="3"
-            class="upper-content">
+          <v-col cols="12">
             <v-row>
               <v-col
                 cols="12"
-                class="pt-0">
-                <b-card>
-                  <h3 class="headline darker--text">Nos réseaux sociaux</h3>
-                  <p>Nous postons régulièrement des nouvelles, des tips et des astuces. Suivez-nous!</p>
-                  <social-icons big/>
-                </b-card>
-              </v-col>
-              <v-col
-                cols="12"
-                class="pt-0">
-                <b-card>
-                  <h3 class="headline darker--text">À propos</h3>
-                  <p>BecauseOfProg, c'est des développeurs rassemblés autour de projets communs tels que le blog.</p>
-                  <template #actions>
-                    <v-btn
-                      to="/page/about"
-                      color="darker"
-                      text>
-                      <v-icon left>mdi-information-outline</v-icon>
-                      En savoir plus
-                    </v-btn>
-                  </template>
-                </b-card>
-              </v-col>
-              <v-col
-                cols="12"
-                class="pt-0">
-                <b-card gradient>
-                  <h3 class="headline white--text">Les liens BecauseOfProg</h3>
-                  <p>Nous partageons des liens vers des ressources, des blogs ou des articles intéressants, afin que vous puissiez vous aussi les découvrir.</p>
-                  <template #actions>
-                    <v-btn
-                      text
-                      color="white"
-                      href="https://twitter.com/BecauseOfProg">
-                      Twitter
-                    </v-btn>
-                    <v-btn
-                      text
-                      color="white"
-                      href="#!">
-                      RSS
-                    </v-btn>
-                  </template>
-                </b-card>
-              </v-col>
-              <v-col
-                cols="12"
-                class="pt-0">
-                <b-card>
-                  <h3 class="headline darker--text">Catégories</h3>
-                  <v-col>
-                    <v-chip
-                      v-for="category in categories"
-                      :key="category.id"
-                      :to="`/category/${category.id}`"
-                      class="mr-1 mb-1">
-                      <v-icon left>{{ category.icon }}</v-icon>
-                      {{ category.name }}
-                    </v-chip>
-                    <v-chip
-                      v-for="type in types"
-                      :key="type.id"
-                      :to="`/type/${type.id}`"
-                      class="mr-1 mb-1">
-                      <v-icon left>{{ type.icon }}</v-icon>
-                      {{ type.name }}
-                    </v-chip>
+                lg="7"
+                offset-lg="1">
+                <v-row>
+                  <v-col
+                    cols="12"
+                    class="pt-0">
+                    <b-card>
+                      <h1>{{ article.author.displayname }}</h1>
+                    </b-card>
                   </v-col>
-                </b-card>
+                </v-row>
               </v-col>
+              <v-col
+                cols="12"
+                lg="3">
+                <v-row>
+                  <v-col
+                    cols="12"
+                    class="pt-0">
+                    <b-card>
+                      <h3 class="headline darker--text">Nos réseaux sociaux</h3>
+                      <p>Nous postons régulièrement des nouvelles, des tips et des astuces. Suivez-nous!</p>
+                      <social-icons big/>
+                    </b-card>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    class="pt-0">
+                    <b-card>
+                      <h3 class="headline darker--text">À propos</h3>
+                      <p>BecauseOfProg, c'est des développeurs rassemblés autour de projets communs tels que le blog.</p>
+                      <template #actions>
+                        <v-btn
+                          to="/page/about"
+                          color="darker"
+                          text>
+                          <v-icon left>mdi-information-outline</v-icon>
+                          En savoir plus
+                        </v-btn>
+                      </template>
+                    </b-card>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    class="pt-0">
+                    <b-card gradient>
+                      <h3 class="headline white--text">Les liens BecauseOfProg</h3>
+                      <p>Nous partageons des liens vers des ressources, des blogs ou des articles intéressants, afin que vous puissiez vous aussi les découvrir.</p>
+                      <template #actions>
+                        <v-btn
+                          text
+                          color="white"
+                          href="https://twitter.com/BecauseOfProg">
+                          Twitter
+                        </v-btn>
+                        <v-btn
+                          text
+                          color="white"
+                          href="#!">
+                          RSS
+                        </v-btn>
+                      </template>
+                    </b-card>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    class="pt-0">
+                    <b-card>
+                      <h3 class="headline darker--text">Catégories</h3>
+                      <v-col>
+                        <v-chip
+                          v-for="category in categories"
+                          :key="category.id"
+                          :to="`/category/${category.id}`"
+                          class="mr-1 mb-1">
+                          <v-icon left>{{ category.icon }}</v-icon>
+                          {{ category.name }}
+                        </v-chip>
+                        <v-chip
+                          v-for="type in types"
+                          :key="type.id"
+                          :to="`/type/${type.id}`"
+                          class="mr-1 mb-1">
+                          <v-icon left>{{ type.icon }}</v-icon>
+                          {{ type.name }}
+                        </v-chip>
+                      </v-col>
+                    </b-card>
+                  </v-col>
+                </v-row>
+              </v-col>
+
             </v-row>
           </v-col>
         </v-row>
