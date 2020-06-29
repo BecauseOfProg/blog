df24e7f2a66617c56625cd984ed531a90fe63e1f
Théo Vidal <exybore@becauseofprog.fr>
2020-06-29 23:22:14 +0200

    fix: fix API url
    Fixed the URL since the version 2.2 is now in production

--- src/utils/data/settings.js
+++ src/utils/data/settings.js
@@ -1,5 +1,5 @@
 const settings = {
-  api: 'http://127.0.0.1:5000/v1',
+  api: 'https://api.becauseofprog.fr/v1',
   rss_base: 'https://gh.becauseofprog.fr/rss-proxy/',
   selected_article: 'linux-sur-android'
 }
