d09ce1395927f9e3bdbb33a4bbfcbb2810bc2591
Théo Vidal <exybore@becauseofprog.fr>
2020-07-25 21:52:22 +0200

    feat: add projects page

--- src/utils/data/projects.js
+++ src/utils/data/projects.js
@@ -1,28 +1,36 @@
 const projects = [
   {
     name: 'Youtube Podcast',
-    link: 'https://podcast.becauseofprog.fr',
+    links: [
+      { name: 'website', value: 'https://podcast.becauseofprog.fr' },
+      { name: 'source', value: 'https://github.com/BecauseOfProg/yt-podcast' }
+    ],
     description: 'Transformez facilement une chaîne YouTube ou une playlist en podcast audio ou vidéo'
   },
   {
-    name: 'Nos projets sur Github',
-    link: 'https://github.com/BecauseOfProg',
-    description: 'Retrouvez-nous sur GitHub, nos principaux projets y sont disponibles !'
-  },
-  {
     name: 'Notre API',
-    link: 'https://github.com/BecauseOfProg/api-docs',
+    links: [
+      { name: 'documentation', value: 'https://github.com/BecauseOfProg/api-docs' },
+      { name: 'source', value: 'https://github.com/BecauseOfProg/api' }
+    ],
     description: 'La documentation de notre API publique est disponible sur GitHub. #OpenData'
   },
   {
     name: 'Notre robot Discord',
-    link: 'https://github.com/BecauseOfProg/boite-a-bois',
-    description: 'bip-bip, utilisateur banni! Le code source de notre bot est disponible'
+    links: [
+      { name: 'discord', value: 'https://discord.becauseofprog.fr' },
+      { name: 'source', value: 'https://github.com/BecauseOfProg/boite-a-bois' }
+    ],
+    description:
+      'bip-bip, utilisateur banni! Nous possédons un robot sur notre serveur Discord développé par nos soins!'
   },
   {
     name: 'Une implémentation de l\'API d\'OpenWeatherMap en Ruby',
-    link: 'https://github.com/BecauseOfProg/openweathermap-ruby',
-    description: 'Une librairie rapide, simple et documentée pour accéder facilement aux données d\'OpenWeatherMap'
+    links: [
+      { name: 'source', value: 'https://github.com/BecauseOfProg/openweathermap-ruby' }
+    ],
+    description:
+      'Une librairie rapide, simple et documentée pour accéder facilement aux données d\'OpenWeatherMap en utilisant la flexibilité du langage Ruby'
   }
 ]
 
--- src/views/page/Projects.vue
+++ src/views/page/Projects.vue
@@ -3,12 +3,110 @@
     <b-top-banner
       title="global.projects"
       icon="mdi-package-variant"/>
-    <v-container class="page-body"/>
+    <v-container class="page-body">
+      <v-row>
+        <v-col
+          cols="12"
+          lg="7"
+          offset-lg="1">
+          <v-row>
+            <v-col
+              v-for="project in projects"
+              :key="project.name"
+              cols="12"
+              md="6">
+              <b-card>
+                <h3 class="headline">{{ project.name }}</h3>
+                <p class="text-justify">{{ project.description }}</p>
+                <template #actions>
+                  <v-btn
+                    v-for="link in project.links"
+                    :key="project.name + link.name"
+                    :href="link.value"
+                    target="_blank"
+                    color="darker"
+                    text>
+                    <v-icon left>{{ icons[link.name] }}</v-icon>
+                    {{ $t(`projects.links.${link.name}`) }}
+                  </v-btn>
+                </template>
+              </b-card>
+            </v-col>
+            <v-col
+              class="text-right"
+              cols="12">
+              <v-btn
+                href="https://github.com/BecauseOfProg"
+                target="_blank"
+                class="white--text"
+                color="darker">
+                <v-icon left>mdi-github</v-icon>
+                {{ $t('projects.more') }}
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col
+          cols="12"
+          lg="3">
+          <v-col cols="12">
+            <b-card gradient>
+              <h3 class="headline white--text">{{ $t('projects.title') }}</h3>
+              <p class="text-justify">{{ $t('projects.description') }}</p>
+              <template #actions>
+                <v-btn
+                  text
+                  color="white"
+                  href="https://github.com/BecauseOfProg">
+                  GitHub
+                </v-btn>
+                <v-btn
+                  text
+                  color="white"
+                  href="https://twitter.com/BecauseOfProg">
+                  Twitter
+                </v-btn>
+              </template>
+            </b-card>
+          </v-col>
+          <v-col
+            cols="12"
+            class="pt-0">
+            <b-card>
+              <h3 class="headline">{{ $t('projects.blog.title') }}</h3>
+              <p class="text-justify">{{ $t('projects.blog.description') }}</p>
+              <template #actions>
+                <v-btn
+                  href="https://github.com/BecauseOfProg/blog"
+                  target="_blank"
+                  color="darker"
+                  text>
+                  {{ $t('projects.blog.discover') }}
+                </v-btn>
+              </template>
+            </b-card>
+          </v-col>
+        </v-col>
+      </v-row>
+    </v-container>
   </main>
 </template>
 
 <script>
+import { projects } from '@/utils/data'
+
 export default {
-  name: 'Projects'
+  name: 'Projects',
+  data() {
+    return {
+      icons: {
+        documentation: 'mdi-text-box-multiple-outline',
+        source: 'mdi-xml',
+        discord: 'mdi-discord',
+        website: 'mdi-web'
+      },
+      projects
+    }
+  }
 }
 </script>
