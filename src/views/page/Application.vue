7acb1786494d165620fe9a24ca3b01ec1888ccbe
Théo Vidal <exybore@becauseofprog.fr>
2020-07-27 20:17:38 +0200

    feat: add application page

--- src/views/page/Application.vue
+++ src/views/page/Application.vue
@@ -1,14 +1,152 @@
 <template>
   <main>
     <b-top-banner
-      title="global.application"
-      icon="mdi-cellphone-iphone"/>
-    <v-container class="page-body"/>
+      title="application.title"
+      subtitle="application.subtitle"
+      tall
+      image>
+      <ApplicationDownload/>
+    </b-top-banner>
+    <div
+      v-for="(part, i) in imagedParts"
+      :key="part.name"
+      :class="{ sheet: i % 2 }"
+      class="page-body">
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            md="3"
+            :offset-md="1 - i % 2"
+            :order="i % 2 === 0 ? 1 : ($vuetify.breakpoint.mdAndUp ? 12 : 1)">
+            <v-img
+              :src="part.image"
+              :alt="$t(`application.${part.name}.title`)"/>
+          </v-col>
+          <v-col
+            cols="12"
+            md="7"
+            :offset-md="i % 2"
+            :order="i % 2 === 0 ? 12 : ($vuetify.breakpoint.mdAndUp ? 1 : 12)">
+            <v-row
+              style="height: 100%"
+              align="center">
+              <v-col cols="12">
+                <p class="text-h4 mb-5">{{ $t(`application.${part.name}.title`) }}</p>
+                <p
+                  class="text-justify"
+                  v-html="$t(`application.${part.name}.description`)"/>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+    <div class="page-body sheet">
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            md="8"
+            offset-md="2">
+            <p class="text-h3 text-center">{{ $t('application.privacy.title') }}</p>
+            <p
+              class="text-justify"
+              v-html="$t('application.privacy.description')"/>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+    <div class="page-body gradient">
+      <div class="centered-container py-16 text-center white--text">
+        <p class="text-h2">{{ $t('application.download.title') }}</p>
+        <p
+          class="text-h4"
+          v-html="$t('application.download.description')"/>
+        <ApplicationDownload/>
+      </div>
+    </div>
+    <v-container class="page-body">
+      <v-row>
+        <v-col
+          cols="12"
+          md="8"
+          offset-md="2">
+          <p class="text-h3 text-center">{{ $t('application.pwa.title') }}</p>
+          <p
+            class="text-justify"
+            v-html="$t('application.pwa.description')"/>
+          <v-list>
+            <template v-for="(browser, i) in browsers">
+              <v-list-item :key="browser.name + 'item'">
+                <v-list-item-icon>
+                  <v-icon color="light">{{ browser.icon }}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title class="mb-2">{{ $t(`application.pwa.browsers.${browser.name}.title`) }}</v-list-item-title>
+                  <span v-html="$t(`application.pwa.browsers.${browser.name}.content`)"/>
+                </v-list-item-content>
+              </v-list-item>
+              <v-divider
+                v-if="i + 1 !== browsers.length"
+                :key="browser.name + 'divider'"/>
+            </template>
+          </v-list>
+        </v-col>
+      </v-row>
+    </v-container>
   </main>
 </template>
 
 <script>
+import ApplicationDownload from './ApplicationDownload'
+
 export default {
-  name: 'Application'
+  name: 'Application',
+  components: { ApplicationDownload },
+  data() {
+    return {
+      imagedParts: [
+        {
+          name: 'list',
+          image: 'https://cdn.becauseofprog.fr/v2/sites/becauseofprog.fr/assets/app/app_home_light.png'
+        },
+        {
+          name: 'article',
+          image: 'https://cdn.becauseofprog.fr/v2/sites/becauseofprog.fr/assets/app/app_article_light.png'
+        },
+        {
+          name: 'theme',
+          image: 'https://cdn.becauseofprog.fr/v2/sites/becauseofprog.fr/assets/app/app_home_dark.png'
+        }
+      ],
+      browsers: [
+        {
+          name: 'chrome',
+          icon: 'mdi-google-chrome'
+        },
+        {
+          name: 'android',
+          icon: 'mdi-android'
+        },
+        {
+          name: 'firefox',
+          icon: 'mdi-firefox'
+        },
+        {
+          name: 'opera',
+          icon: 'mdi-opera'
+        },
+        {
+          name: 'opera_mini',
+          icon: 'mdi-opera'
+        },
+        {
+          name: 'safari',
+          icon: 'mdi-apple-safari'
+        },
+      ]
+    }
+  }
 }
 </script>
---
+++ src/views/page/ApplicationDownload.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-row
+    class="mt-5"
+    justify="space-around">
+    <v-col>
+      <v-btn
+        :href="universal"
+        color="green darken-1 white--text">
+        <v-icon left>mdi-android</v-icon>
+        Télécharger ≈18Mo
+      </v-btn>
+    </v-col>
+    <v-col>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="green lighten-2 white--text"
+            v-bind="attrs"
+            v-on="on">
+            Autres architectures ≈9Mo
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="arch in archs"
+            :key="arch"
+            :href="`https://cdn.becauseofprog.fr/v2/sites/becauseofprog.fr/app/app-${arch}-release.apk`">
+            <v-list-item-title>{{ arch }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'ApplicationDownload',
+  data() {
+    return {
+      universal: 'https://cdn.becauseofprog.fr/v2/sites/becauseofprog.fr/app/app-universal-release.apk',
+      archs: [
+        'armeabi-v7a',
+        'arm64-v8a',
+        'x86',
+        'x86_64'
+      ]
+    }
+  }
+}
+</script>
