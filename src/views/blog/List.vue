0d076157a8859d38a45d5cd62f92167f56499db1
Théo Vidal <exybore@becauseofprog.fr>
2020-08-03 20:24:42 +0200

    feat: add infinite scroll w/ button and fab to return up

--- src/views/blog/List.vue
+++ src/views/blog/List.vue
@@ -12,14 +12,8 @@
           cols="12"
           lg="12">
           <categories-chips include-all/>
-          <v-pagination
-            v-if="pages > 0"
-            v-model="page"
-            :length="pages"
-            color="darker"
-            class="mt-3 mb-2"/>
-          <v-scale-transition>
-            <template v-if="loaded">
+          <template v-if="articles.length">
+            <v-scale-transition>
               <v-row>
                 <v-col
                   v-for="(article, index) in articles"
@@ -53,10 +47,22 @@
                   </template>
                 </v-col>
               </v-row>
-            </template>
-          </v-scale-transition>
-          <v-fade-transition appear>
-            <template v-if="!loaded">
+            </v-scale-transition>
+            <div class="text-center">
+              <v-btn
+                v-if="params.page !== pages"
+                color="darker"
+                text
+                :loading="loading"
+                @click="fetchArticles">
+                Voir plus
+              </v-btn>
+            </div>
+          </template>
+          <v-fade-transition
+            v-else
+            appear>
+            <template>
               <div class="mt-3">
                 <v-skeleton-loader
                   style="width: 100%"
@@ -65,79 +71,40 @@
             </template>
           </v-fade-transition>
         </v-col>
-        tout ce qui a après vous vous en occupez pas ça va dégager
         <v-col
           cols="12"
-          lg="3"
-          offset-lg="1">
-          <b-card
-            fluid
-            tile
-            class="bordered">
-            <v-list
-              class="round"
-              subheader
-              dense
-              tile>
-              <v-list-item
-                :to="{ name: 'all-articles' }"
-                color="darker">
-                <v-list-item-icon>
-                  <v-icon>mdi-text-box-multiple-outline</v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                  <v-list-item-title>{{ $t('global.allArticles') }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-              <v-divider/>
-              <v-subheader>{{ $t('list.categoriesCaps') }}</v-subheader>
-              <v-list-item
-                v-for="category in categories"
-                :key="category.id"
-                :to="{ name: 'category', params: { category: category.id }}"
-                color="darker">
-                <v-list-item-icon>
-                  <v-icon>{{ category.icon }}</v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                  <v-list-item-title>{{ $t(`categories.${category.id}`) }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-              <v-divider/>
-              <v-subheader>{{ $t('list.typesCaps') }}</v-subheader>
-              <v-list-item
-                v-for="type in types"
-                :key="type.id"
-                :to="{ name: 'type', params: { type: type.id }}"
-                color="darker">
-                <v-list-item-icon>
-                  <v-icon>{{ type.icon }}</v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                  <v-list-item-title>{{ $t(`types.${type.id}`) }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list>
-            <v-divider/>
-            <div class="pa-3">
-              <h3 class="headline">{{ $t('list.followUs') }}</h3>
-              <social-icons big/>
-            </div>
-            <v-divider/>
-            <div class="pa-3">
-              <h3 class="headline">{{ $t('global.about') }}</h3>
-              <p>{{ $t('global.aboutMessage') }}</p><br>
-              <v-btn
-                text
-                color="darker"
-                :to="{ name: 'about' }">
-                <v-icon left>mdi-information-outline</v-icon>
-                {{ $t('global.more') }}
-              </v-btn>
-            </div>
+          lg="8"
+          offset-lg="2">
+          <b-card>
+            <v-row>
+              <v-col cols="6">
+                <h3 class="headline">{{ $t('list.followUs') }}</h3>
+                <social-icons big/>
+              </v-col>
+              <v-col cols="6">
+                <h3 class="headline">{{ $t('global.about') }}</h3>
+                <p>{{ $t('global.aboutMessage') }}</p><br>
+                <v-btn
+                  text
+                  color="darker"
+                  :to="{ name: 'about' }">
+                  <v-icon left>mdi-information-outline</v-icon>
+                  {{ $t('global.more') }}
+                </v-btn>
+              </v-col>
+            </v-row>
           </b-card>
         </v-col>
       </v-row>
+      <v-btn
+        color="light white--text"
+        fab
+        fixed
+        bottom
+        right
+        @click="$vuetify.goTo('.b-top-banner')">
+        <v-icon>mdi-chevron-up</v-icon>
+      </v-btn>
     </v-container>
   </main>
 </template>
@@ -159,9 +126,11 @@ export default {
         icon: '',
         image: undefined
       },
+      params: {
+        page: 0
+      },
       articles: [],
-      loaded: false,
-      page: 1,
+      loading: false,
       pages: 0,
 
       categories,
@@ -169,38 +138,41 @@ export default {
     }
   },
   mounted() {
+    if (this.$route.params.category) {
+      this.params = {
+        ...this.params,
+        category: this.$route.params.category
+      }
+      let category = getCategory(this.$route.params.category)
+      this.head.title = ['list.category', { category: this.$t(`categories.${category.id}`) }]
+      this.head.icon = category.icon
+      this.head.image = `/img/category/${category.id}.png`
+    } else if (this.$route.params.type) {
+      this.params = {
+        ...this.params,
+        type: this.$route.params.type
+      }
+      let type = getType(this.$route.params.type)
+      this.head.title = ['list.type', { type: this.$t(`types.${type.id}`) }]
+      this.head.icon = type.icon
+      this.head.image = `/img/type/${type.id}.png`
+    } else {
+      this.head.title = ['global.allArticles']
+      this.head.icon = 'mdi-text-box-multiple-outline'
+    }
     this.fetchArticles()
   },
   methods: {
     fetchArticles() {
-      this.loaded = false
-      let params = {
-        page: this.page
-      }
-
-      if (this.$route.params.category) {
-        params['category'] = this.$route.params.category
-        let category = getCategory(this.$route.params.category)
-        this.head.title = ['list.category', { category: this.$t(`categories.${category.id}`) }]
-        this.head.icon = category.icon
-        this.head.image = `/img/category/${category.id}.png`
-      } else if (this.$route.params.type) {
-        params['type'] = this.$route.params.type
-        let type = getType(this.$route.params.type)
-        this.head.title = ['list.type', { type: this.$t(`types.${type.id}`) }]
-        this.head.icon = type.icon
-        this.head.image = `/img/type/${type.id}.png`
-      } else {
-        this.head.title = ['global.allArticles']
-        this.head.icon = 'mdi-text-box-multiple-outline'
-      }
-
-      blogPosts.get(params).then(data => {
-        if (params.page === this.page) {
-          this.articles = data.body.data
-          this.pages = data.body.pages
-          this.loaded = true
-        }
+      this.loading = true
+      this.params.page += 1
+      blogPosts.get(this.params).then(data => {
+        this.articles = [
+          ...this.articles,
+          ...data.body.data
+        ]
+        this.pages = data.body.pages
+        this.loading = false
       })
     }
   },
