6cfec89cbace09a56c327fefaf6fdb4abe992db0
kernoeb <kernoeb@protonmail.com>
2021-07-18 13:48:59 +0200

    chore: change plausible URL

--- src/plugins/plausible.js
+++ src/plugins/plausible.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import { VuePlausible } from 'vue-plausible'
 
 Vue.use(VuePlausible, {
-    apiHost: 'https://plausible.becauseofprog.fr'
+    apiHost: 'https://plausible.noewen.com'
 })
 
 Vue.$plausible.enableAutoPageviews() // optional
